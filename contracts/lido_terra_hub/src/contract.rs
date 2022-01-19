e4aa838fbff9a5eb70b438cfe6cc7821d96276cc
pr0n00gler <programmer10110@yandex.ru>
2022-01-19 14:34:43 +0300

    rust version in ci updated

--- contracts/lido_terra_hub/src/contract.rs
+++ contracts/lido_terra_hub/src/contract.rs
@@ -257,12 +257,14 @@ pub fn execute_dispatch_rewards(
                 .addr_humanize(&config.airdrop_registry_contract.ok_or_else(|| {
                     StdError::generic_err("the airdrop registry contract must have been registered")
                 })?)?;
-        for msg in airdrop_hooks.unwrap() {
-            messages.push(CosmosMsg::Wasm(WasmMsg::Execute {
-                contract_addr: registry_addr.to_string(),
-                msg,
-                funds: vec![],
-            }))
+        if let Some(hook) = airdrop_hooks {
+            for msg in hook {
+                messages.push(CosmosMsg::Wasm(WasmMsg::Execute {
+                    contract_addr: registry_addr.to_string(),
+                    msg,
+                    funds: vec![],
+                }))
+            }
         }
     }
 
